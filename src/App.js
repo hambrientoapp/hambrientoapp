9b9caca61190ab6707bbbefdfaa7e42fe9c81625
Alejandro Melo Meza <ale.melo.meza@icloud.com>
2019-09-26 18:14:04 -0300

    feat(public): páginas estátitcas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path="/" exact component={ Home } />
+        <Route path="/about" component={ About } />
+        <Route path="/contact" component={ Contact} />
+      </Switch>
+    </Router> 
+  )
 }
 
-export default App;
+export default App
