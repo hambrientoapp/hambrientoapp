b27b49e728f13d10b1f2eab16667a34c97587f22
Alejandro Melo Meza <ale.melo.meza@icloud.com>
2019-10-03 12:41:47 -0300

    feat(front-end): center content, add margin bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import { Container } from 'react-bootstrap'
 
 export default function Footer() {
     return (
-        <Container className="mt-4">
-            <footer style={{ borderTop: '20px' }}>
-                <span role='img' aria-label='burger emoji' className="text-center">🍔 <br /> &copy; HAMBRIENTOapp</span>
+        <Container className="mt-4 mb-4">
+            <footer  className="text-center">
+                <span role='img' aria-label='burger emoji'>🍔 <br /> &copy; HAMBRIENTOapp</span>
             </footer>
         </Container>
     )
