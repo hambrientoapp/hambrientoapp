b748c90ed861a488f51d1340530cad1a28632644
yramirez <yramirez@mma.gob.cl>
2019-10-03 11:21:00 -0300

    change about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -42,8 +42,8 @@ function About() {
                         <Card className="mb-2">
                             <Card.Img variant="top" src="https://via.placeholder.com/100" />
                              <Card.Body className="text-center">
-                                <Card.Title>User Three</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">@userthree</Card.Subtitle>
+                                <Card.Title>Yoselyn Ramirez</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">@esmeraldarr</Card.Subtitle>
                                 <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias eaque, architecto saepe optio reprehenderit soluta adipisci suscipit impedit laboriosam dolorum accusantium ea iste recusandae. Odio hic fugit facere pariatur?</Card.Text>
                             </Card.Body>
                         </Card>
